28c18cfdc6e3b284351ba97f206d1fc2d982b102
B.Khalil <bkhalil4797@gmail.com>
2021-08-31 09:46:47 +0100

    test speech synthesis

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export const Settings = () => {
+  useEffect(() => {
+    let msg = new SpeechSynthesisUtterance(
+      "Vous étes sur la page des parametres"
+    );
+    const voices = speechSynthesis.getVoices();
+    msg.voice = voices[9];
+    window.speechSynthesis.speak(msg);
+    msg = new SpeechSynthesisUtterance("Merci d'avoir utilisé notre service");
+    msg.voice = voices[9];
+    window.speechSynthesis.speak(msg);
+    console.log(window.speechSynthesis.getVoices());
+  }, []);
   return (
     <>
       <h1>Settings</h1>
